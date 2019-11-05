7a0c9c6edf0e2aea349887915c4f6eb3f35adf4b
Kai Shields <kai@zipcoders-MacBook-Pro-3.local>
2019-11-05 15:45:08 -0500

    checkpoint

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,47 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+    public int compare(Item o1, Item o2) {
+        return 0;
+    }
+
+    public Comparator<Item> reversed() {
+        return null;
+    }
+
+    public Comparator<Item> thenComparing(Comparator<? super Item> other) {
+        return null;
+    }
+
+    public <U> Comparator<Item> thenComparing(Function<? super Item, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        return null;
+    }
+
+    public <U extends Comparable<? super U>> Comparator<Item> thenComparing(Function<? super Item, ? extends U> keyExtractor) {
+        return null;
+    }
+
+    public Comparator<Item> thenComparingInt(ToIntFunction<? super Item> keyExtractor) {
+        return null;
+    }
+
+    public Comparator<Item> thenComparingLong(ToLongFunction<? super Item> keyExtractor) {
+        return null;
+    }
+
+    public Comparator<Item> thenComparingDouble(ToDoubleFunction<? super Item> keyExtractor) {
+        return null;
+    }
 }
--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,54 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item o1, Item o2) {
+        return 0;
+    }
+
+    @Override
+    public Comparator<Item> reversed() {
+        return null;
+    }
+
+    @Override
+    public Comparator<Item> thenComparing(Comparator<? super Item> other) {
+        return null;
+    }
+
+    @Override
+    public <U> Comparator<Item> thenComparing(Function<? super Item, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        return null;
+    }
+
+    @Override
+    public <U extends Comparable<? super U>> Comparator<Item> thenComparing(Function<? super Item, ? extends U> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Item> thenComparingInt(ToIntFunction<? super Item> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Item> thenComparingLong(ToLongFunction<? super Item> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Item> thenComparingDouble(ToDoubleFunction<? super Item> keyExtractor) {
+        return null;
+    }
 }
