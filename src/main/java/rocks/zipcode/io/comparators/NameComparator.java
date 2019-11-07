aa8436f9ba8127bb11cb2cfb7949abdc12f4ca46
MikeGray31 <michaelkgray31@gmail.com>
2019-11-07 16:31:38 -0500

    finished

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,14 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item item, Item t1) {
+        return item.getName().compareTo(t1.getName());
+    }
 }
