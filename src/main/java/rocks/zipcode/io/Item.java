a319f7977e20d1b0856e303844ddb3c5d2a86038
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-10 17:57:05 -0500

    add logic for item class

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,43 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    private String userName;
+    private Long id;
+    private Double itemPrice;
+
     public Item() {
     }
 
     public Item(Long id, String name, Double price) {
+        this.userName = name;
+        this.id = id;
+        this.itemPrice = price;
     }
 
     public String getName() {
-        return null;
+
+        return this.userName;
     }
 
     public void setName(String name) {
+        this.userName = name;
     }
 
     public Long getId() {
-        return null;
+
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+
+        return this.itemPrice;
     }
 
     public void setPrice(Double price) {
+        this.itemPrice = price;
     }
 }
