538b6e64b97536590b6a0007f4dcde60d1489dde
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-11 08:28:50 -0500

    add logic for negative compare test

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,15 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+    public int compare(Item firstItem, Item secondItem) {
+        return firstItem.getId().compareTo(secondItem.getId());
+    }
 }
